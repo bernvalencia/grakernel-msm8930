2fba824c7b3a5e7adee159e67843d0172dfb6d88
Willi Ye <williye97@gmail.com>
2014-01-04 19:24:28 +0100

    remove anykernel format (does not boot)
    Signed-off-by: Willi Ye <williye97@gmail.com>

--- ramdisk/build.sh
+++ ramdisk/build.sh
@@ -1,11 +1,25 @@
-#!/bin/bash
+#!/bin/sh
 
 rm -rf kernel.zip
+rm -rf ramdisk.gz
 find -name "*~" -exec rm -rf {} \;
-find -name ".DS_Store" -exec rm -rf {} \;
 
+build () {
+    cd boot.img-ramdisk
+    find . | cpio -o -H newc | gzip > ../ramdisk.gz
+    cd ..
+    ./mkbootimg-$1 --kernel zImage --ramdisk ramdisk.gz --base 0x80200000 --ramdisk_offset 0x02000000 --cmdline "console=null androidboot.hardware=qcom user_debug=31 zcache" --pagesize 2048 -o boot.img
+}
+
+if [ -e ~/.bash_profile ]; then
+    build darwin
+else
+    build linux
+fi
+
+mv -v boot.img out/
 cd out
-zip -r kernel.zip *
+zip -r kernel.zip META-INF boot.img
 mv -v kernel.zip ../
 cd ..
-adb push kernel.zip /sdcard/.
+adb push kernel.zip /sdcard/
